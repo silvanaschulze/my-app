5a844aca53cfce980fcfa07341e9ffd66c583da6
silvanaschulze <silvanaschulze87@gmail.com>
2021-03-10 17:06:49 +0100

    react-router

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import { switch, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+import SingleArticle from './components/SingleArticle';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+ 
+const App = () => {
+  return( 
+    <div>
+      <Navbar />
+      <Switch>
+      <Route exact path='/' component={Home} />
+      <Route path='/article' component={Articles} />
+      <Route path='/article/:id' component={SingleArticle} />
+      </Switch>
+    </div>;
   );
-}
+};
 
 export default App;
